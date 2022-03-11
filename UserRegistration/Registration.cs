e808b9fb1eeeb8c59d4aa051841947d80f7ef86c
GOPAL THAKUR <gopalk557@gmail.com>
2022-03-11 23:48:45 +0530

    [Gopal]Add:Last Name

--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -11,6 +11,7 @@ namespace UserRegistration
     {
         public void UserData()
         {
+            //UCase 1
             Regex reg = new Regex("^[A-Z]{1}[a-z]{2,12}$");
 
             Console.WriteLine("Enter FirstName");
@@ -25,6 +26,20 @@ namespace UserRegistration
                 Console.WriteLine("Please Enter valid Name!");
             }
 
+            //UCase 2
+            Regex reg1 = new Regex("^[A-Z]{1}[a-z]{3,12}$");
+
+            Console.WriteLine("Enter LastName");
+            string lname = Console.ReadLine();
+
+            if (reg1.IsMatch(lname))
+            {
+                Console.WriteLine("Your Name is: " + lname);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter valid Name!");
+            }
 
         }
     }
